a37c9a25a67e908d7c34d0623b44158c89a05427
prashant133 <dsrprasant@gmail.com>
2023-08-25 20:02:10 +0545

    connection to the db

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log("test")
+const express = require('express')
+const mongoose  = require('mongoose')
+const dotenv = require('dotenv').config()
+
+const app = express()
+
+// loding the env data
+db_conn = process.env.MONGO_URL
+PORT = process.env.PORT
+
+
+// initial routes
+app.get('/',(req , res , next)=> {
+    res.send("servering is running");
+})
+
+
+
+
+// connecting to the db
+mongoose
+    .connect(db_conn)
+    .then(()=>{
+        app.listen(PORT, ()=>{
+            console.log(`server running on port ${PORT}`);
+        })
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+
