809a9600eac9eadf5a3981860ad0f610102722e0
prashant133 <dsrprasant@gmail.com>
2023-09-19 21:49:35 +0545

    edit post functionality

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require('express-async-handler');
 const Post = require('../models/postModel');
 const mongoose = require('mongoose')
+const {fileFilter} = require('../utils/fileUpload')
 
 
 // create a post
@@ -61,4 +62,49 @@ const getPost = asyncHandler(async (req, res, next) => {
 });
 
 
-module.exports = {createPost, getAllPost, getPost}
+// update post
+
+const updatePost = asyncHandler(async (req, res, next) => {
+    try {
+        const { description } = req.body;
+        const { id } = req.params;
+
+        // Find the post by id
+        const post = await Post.findById(id);
+
+        // Check if the post is present in the database
+        if (!post) {
+            return res.status(404).json({ message: 'Post not found' });
+        }
+
+        // Update the post's description if provided
+        if (description) {
+            // Check if the updated description length is within your requirements
+            if (description.length > 500) {
+                return res.status(400).json({ message: 'Description must not exceed 500 characters' });
+            }
+            post.description = description;
+        }
+
+    // check the validity of the file uploaded
+        if (!req.file) {
+            return res.status(400).json({ message: 'Please upload a valid image file (JPEG, JPG, PNG).' });
+        }
+        // update the image path 
+        post.image = req.file.path;
+
+        // Save the updated post
+        const updatedPost = await post.save();
+
+        res.status(200).json(updatedPost);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+
+
+
+
+
+module.exports = {createPost, getAllPost, getPost, updatePost}
--- routes/postRoute.js
+++ routes/postRoute.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const {createPost, getAllPost, getPost}= require('../controllers/postController')
-const upload = require('../utils/fileUpload')
+const {createPost, getAllPost, getPost , updatePost}= require('../controllers/postController')
+const upload = require('../utils/fileUpload');
+const { verifyAdmin } = require('../middleware/authoriztion');
 
-router.post('/', upload.single('image'), createPost);
+
+
+// only admin can do some stuffs
+router.post('/',verifyAdmin, upload.single('image'), createPost);
+router.patch('/update/:id', upload.single("image"), updatePost)
+
+
+
 router.get('/get-all-post', getAllPost );
+
 router.get('/:id', getPost );
 
 module.exports = router;
